08de7c5960b14d99da6249a0bcd603b007481c96
Yeaseen Arafat <yeaseenarafat@Yeaseens-MacBook-Pro.local>
2024-11-25 23:00:52 +0600

    fix: birthday like facebook

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose';
 
 // Connecting the mongoose and with it's Promise..
-// const mongoOptions = {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true
-// };
+const mongoOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true
+};
 
 // Mongoose Promise..
 mongoose.Promise = global.Promise;
 async function connectDB(URL: string) {
-    return mongoose.connect(URL);
+    const connection = await mongoose.connect(URL, mongoOptions as mongoose.ConnectOptions);
+    return connection;
 }
 
 export default connectDB;
