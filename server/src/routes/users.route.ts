448e5bd77dd016c17f7d2afbff1a04d8c8ff226a
asadanik <asadanik@Asads-MacBook-Pro.local>
2025-12-08 23:13:13 +0600

    feat: implement rest-api endpoint for suggested users list

--- server/src/routes/users.route.ts
+++ server/src/routes/users.route.ts
@@ -8,6 +8,7 @@ const userController = new UserController();
 // USER ROUTES
 router.route('/').get(userController.showUsers).patch(userController.updateThemeMode);
 router.route('/me').get(userController.profileAuth);
+router.get('/follows-suggested', userController.getFollowsSuggested);
 router.route('/:userId').get(userController.profileById);
 router.patch('/update-photo', imageUpload.single("file"), userController.uploadProfilePic);
 router.patch('/update-cover', imageUpload.single("file"), userController.uploadCoverPic);
